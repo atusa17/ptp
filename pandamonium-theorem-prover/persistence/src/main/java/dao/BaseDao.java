52e08d61d08e222cfda99a30227b07bb4872bccb
atusa17 <alex.j.tusa@gmail.com>
2019-02-03 19:07:25 -0700

    Set up the BaseDAO

--- pandamonium-theorem-prover/persistence/src/main/java/dao/BaseDao.java
+++ pandamonium-theorem-prover/persistence/src/main/java/dao/BaseDao.java
@@ -1,4 +1,12 @@
 package dao;
 
-public class BaseDao {
+class BaseDao {
+
+    <T> void save(final T t) {
+        //TODO insert DB calls here for insert and update
+    }
+
+    <T> void delete(final T t) {
+        //TODO insert DB calls here to delete
+    }
 }
