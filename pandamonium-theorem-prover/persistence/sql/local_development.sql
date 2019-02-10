89af852ec8d080fee1fae03c1a5b94f44d28079f
atusa17 <alex.j.tusa@gmail.com>
2019-02-10 11:38:02 -0700

    PAN-6 Created the DB script and updated the .travis file to create the DB in Travis

--- pandamonium-theorem-prover/persistence/sql/local_development.sql
+++ pandamonium-theorem-prover/persistence/sql/local_development.sql
@@ -1,13 +1,13 @@
 create database pandamonium;
 use pandamonium;
-create table accounts {
+create table accounts (
 id int not null auto_increment primary key unique,
 username varchar(50) not null unique,
 password varchar(256) not null,
 administrator_status boolean default false,
 last_login date,
 version int default 0
-};
+);
 insert into accounts (username, password, administrator_status)
 values ('admin', 'secret', true),
 ('atusa', 'secret', true),
@@ -15,6 +15,10 @@ values ('admin', 'secret', true),
 ('BrittanyBi', 'secret', true),
 ('lanlanzeliu', 'secret', true),
 ('tramanh305', 'secret', true);
-create table definitions {
-
-}
+create table definitions (
+id int not null auto_increment primary key unique,
+name varchar(200) not null,
+definition json not null,
+notation json,
+version int default 0
+);
