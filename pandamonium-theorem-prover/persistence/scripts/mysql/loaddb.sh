1e77a38483d72430dd5936a3313db82f2c0f229e
atusa17 <alex.j.tusa@gmail.com>
2019-02-10 12:16:58 -0700

    PAN-6 Added the loadDb task to the build pipeline for Travis

--- pandamonium-theorem-prover/persistence/scripts/mysql/loaddb.sh
+++ pandamonium-theorem-prover/persistence/scripts/mysql/loaddb.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export MYSQL_PWD=secret
 
-for sqlScript in $( find ../../ -name "*.sql" -print | sort);
+for sqlScript in $( find . -name "*.sql" -print | sort);
    do
       echo "**** $sqlScript ****"
       mysql --batch --quick --raw --line-numbers --force --user=panda < $sqlScript
