e63a9b99da96f86f496511c1250a3dd8315547a4
Brittany <biancobrittany@yahoo.com>
2019-02-20 21:09:56 -0700

    Delete TheoremRepository.java

--- pandamonium-theorem-prover/persistence/src/main/java/edu/msudenver/tsp/persistence/repository/TheoremRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.msudenver.tsp.persistence.repository;
-
-import edu.msudenver.tsp.persistence.dto.BaseDto;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TheoremRepository extends CrudRepository<BaseDto, Long> {
-}
