bb28de709ae80d682fddb027ede31c99439c027f
atusa17 <alex.j.tusa@gmail.com>
2019-02-24 16:26:17 -0700

    PAN-7 Updated the loaddb.sh file

--- pandamonium-theorem-prover/persistence/scripts/mysql/loaddb.sh
+++ pandamonium-theorem-prover/persistence/scripts/mysql/loaddb.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-export MYSQL_PWD=secret
 
 for sqlScript in $( find . -name "*.sql" -print | sort);
    do
       echo "**** $sqlScript ****"
-      mysql --batch --quick --raw --line-numbers --force --user=panda < $sqlScript
+      mysql -u panda -psecret < $sqlScript
    done
