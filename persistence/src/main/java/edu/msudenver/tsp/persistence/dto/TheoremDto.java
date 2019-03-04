9d7673ebf2b1716bd2e0d760a85ff3b68155ce08
atusa17 <alex.j.tusa@gmail.com>
2019-03-03 18:36:14 -0700

    PAN-50 Created the DTO for theorems

--- persistence/src/main/java/edu/msudenver/tsp/persistence/dto/TheoremDto.java
+++ persistence/src/main/java/edu/msudenver/tsp/persistence/dto/TheoremDto.java
@@ -1,4 +1,19 @@
 package edu.msudenver.tsp.persistence.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
 public class TheoremDto extends BaseDto {
+    @NotBlank @Size(min = 1, max = 512, message = "theorem name must be between 1 and 512 characters") private String name;
+    @NotNull @JsonProperty("theorem_type") private TheoremType theoremType;
+    @NotNull private String branch;
+    @Type(type = "json") @Column(columnDefinition = "jsonb") @JsonProperty("referenced_definitions") private List<String> referencedDefinitions;
+    @Type(type = "json") @Column(columnDefinition = "jsonb") @JsonProperty("referenced_theorems") private List<String> referencedTheorems;
+    @NotNull @JsonProperty("proven_status") private boolean provenStatus;
 }
