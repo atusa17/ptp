a0753903b253111e4f0885946be65b37e192c5cf
atusa17 <alex.j.tusa@gmail.com>
2019-03-03 21:12:07 -0700

    PAN-50 Updated the DTO's

--- persistence/src/main/java/edu/msudenver/tsp/persistence/dto/AccountDto.java
+++ persistence/src/main/java/edu/msudenver/tsp/persistence/dto/AccountDto.java
@@ -1,6 +1,5 @@
 package edu.msudenver.tsp.persistence.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -22,8 +21,8 @@ import java.util.Date;
 public class AccountDto extends BaseDto implements Serializable {
     @NotBlank(groups = Insert.class, message = "A username must be specified") @Size(max = 50) private String username;
     @NotBlank(groups = Insert.class, message = "A password must be specified") @Size(max = 256) private String password;
-    @NotNull @JsonProperty("administrator_status") private boolean administratorStatus;
-    @Temporal(TemporalType.DATE) @JsonProperty("last_login") private Date lastLogin;
+    @NotNull private boolean administrator_status;
+    @Temporal(TemporalType.DATE) private Date last_login;
 
     public static final long serialVersionUID = 7095627971593953734L;
 
--- persistence/src/main/java/edu/msudenver/tsp/persistence/dto/TheoremDto.java
+++ persistence/src/main/java/edu/msudenver/tsp/persistence/dto/TheoremDto.java
@@ -1,6 +1,5 @@
 package edu.msudenver.tsp.persistence.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.Type;
@@ -21,11 +20,11 @@ import java.util.List;
 @EqualsAndHashCode(callSuper = true)
 public class TheoremDto extends BaseDto implements Serializable {
     @NotBlank @Size(min = 1, max = 512, message = "theorem name must be between 1 and 512 characters") private String name;
-    @NotNull @JsonProperty("theorem_type") private TheoremType theoremType;
+    @NotNull private TheoremType theorem_type;
     @NotNull(message = "a branch of mathematics that this theorem is associated with must be specified") private String branch;
-    @Type(type = "json") @Column(columnDefinition = "jsonb") @JsonProperty("referenced_definitions") private List<String> referencedDefinitions;
-    @Type(type = "json") @Column(columnDefinition = "jsonb") @JsonProperty("referenced_theorems") private List<String> referencedTheorems;
-    @NotNull @JsonProperty("proven_status") private boolean provenStatus;
+    @Type(type = "json") @Column(columnDefinition = "jsonb") private List<String> referenced_definitions;
+    @Type(type = "json") @Column(columnDefinition = "jsonb") private List<String> referenced_theorems;
+    @NotNull private boolean proven_status;
 
     public static final long serialVersionUID = 1545568391140364425L;
 }
