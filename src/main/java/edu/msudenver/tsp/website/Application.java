f86b36aa1798827d1380b50448956559f667adc1
BrittanyBi <biancobrittany@yahoo.com>
2019-04-07 23:55:15 -0600

    PAN-54 Added call to UserService to query DB.

--- src/main/java/edu/msudenver/tsp/website/Application.java
+++ src/main/java/edu/msudenver/tsp/website/Application.java
@@ -1,12 +1,22 @@
 package edu.msudenver.tsp.website;
 
+import edu.msudenver.tsp.services.RestService;
+import edu.msudenver.tsp.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "edu.msudenver.tsp")
 public class Application {
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    @Autowired
+    public UserService userService(final RestService restService) {
+        return new UserService(restService);
     }
 }
