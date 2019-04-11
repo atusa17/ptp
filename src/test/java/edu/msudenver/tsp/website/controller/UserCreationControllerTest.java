cf49f9e980dab5dfeaabf329132ce773edbe72c9
BrittanyBi <biancobrittany@yahoo.com>
2019-04-10 20:17:20 -0600

    PAN-54 Test.

--- src/test/java/edu/msudenver/tsp/website/controller/UserCreationControllerTest.java
+++ src/test/java/edu/msudenver/tsp/website/controller/UserCreationControllerTest.java
@@ -1,5 +1,6 @@
 package edu.msudenver.tsp.website.controller;
 
+import edu.msudenver.tsp.services.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
@@ -7,14 +8,16 @@ import org.springframework.web.servlet.ModelAndView;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserCreationControllerTest {
 
-    private final UserCreationController userCreationController = new UserCreationController();
+    private final UserService userService = mock(UserService.class);
+    private final UserCreationController userCreationController = new UserCreationController(userService);
 
     @Test
-    public void testUserCreationPage() {
+    public void testCreateUserPage() {
         final ModelAndView modelAndView = userCreationController.createUserPage();
 
         assertNotNull(modelAndView);
