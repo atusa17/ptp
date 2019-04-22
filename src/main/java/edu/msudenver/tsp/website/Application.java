3ee3d1bb06a5b87582fcad9d209a89a46075e972
BrittanyBi <biancobrittany@yahoo.com>
2019-04-22 12:42:28 -0600

    PAN-64 Errors

--- src/main/java/edu/msudenver/tsp/website/Application.java
+++ src/main/java/edu/msudenver/tsp/website/Application.java
@@ -1,11 +1,7 @@
 package edu.msudenver.tsp.website;
 
-import edu.msudenver.tsp.services.RestService;
-import edu.msudenver.tsp.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = "edu.msudenver.tsp")
 public class Application {
@@ -14,9 +10,4 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    @Autowired
-    public UserService userService(final RestService restService) {
-        return new UserService(restService);
-    }
 }
